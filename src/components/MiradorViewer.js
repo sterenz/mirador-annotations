2d5ebb46e7d52ace8931ec678cee5cf3ab95a58c
sterenz <87770613+sterenz@users.noreply.github.com>
2024-03-20 12:04:45 +0100

    Commented `MiradorViewer.js`

--- src/components/MiradorViewer.js
+++ src/components/MiradorViewer.js
@@ -1,38 +1,44 @@
 import React, { useEffect } from 'react';
 import Mirador from 'mirador';
 
-// import plugin
-import annotationPlugins from 'mirador-annotations'
-import LocalStorageAdapter from 'mirador-annotations/lib/LocalStorageAdapter';
+// Import annotation plugin from node_modules
+import annotationPlugins from 'mirador-annotations';
 
-// import LocalStorageAdapter from 'mirador-annotations/lib/LocalStorageAdapter';
+// Import LocalStorageAdapter from local file
+import LocalStorageAdapter from 'mirador-annotations/lib/LocalStorageAdapter';
 
 const MiradorComponent = () => {
   useEffect(() => {
+    // Configuration for Mirador viewer
     const config = {
       annotation: {
+        // Set adapter for annotations to LocalStorageAdapter
         adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
-        // adapter: (canvasId) => new AnnototAdapter(canvasId, endpointUrl),
-        exportLocalStorageAnnotations: true, // display annotation JSON export button
+        // Set to true to display annotation JSON export button
+        exportLocalStorageAnnotations: true,
       },
       window: {
+        // Set default sidebar panel to annotations
         defaultSideBarPanel: 'annotations',
+        // Open sidebar by default
         sideBarOpenByDefault: true,
       },
-      id: 'mirador-container',
+      id: 'mirador-container', // ID of the Mirador container
       windows: [
         {
+          // Enable image tools
           imageToolsEnabled: true,
+          // Manifest ID of the IIIF manifest to load
           manifestId: 'https://iiif.harvardartmuseums.org/manifests/object/299843',
         },
       ],
     };
 
+    // Initialize Mirador viewer with configuration and annotation plugins
     Mirador.viewer(config, [...annotationPlugins]);
-  }, []);
+  }, []); // Run effect only once when component mounts
 
-
-
+  // Render the container for Mirador viewer
   return <div id="mirador-container"></div>;
 };
 
